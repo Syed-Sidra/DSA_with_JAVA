5fe1402160ac708bd458ce442be1cca25eb6d9e7
Syed-Sidra <sn0255724@gmail.com>
2025-07-30 20:33:50 +0530

    Initial commit

---
+++ Maths Basics/PrimeNumber.java
@@ -0,0 +1,36 @@
+class PrimeNumber{
+
+    // Method for prime number
+
+    public static boolean primeNumber(int num){
+        if(num <= 1){
+            return false;
+        }
+
+        if(num == 2 || num == 3){
+            return true;
+        }
+
+        if(num % 2 == 0 || num % 3 == 0){
+            return false;
+        }
+
+        for(int i = 5;i <= Math.sqrt(num); i = i+6){
+            if(num % i == 0 || num % (i+2) == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num = 101;
+       if(primeNumber(num)){
+        System.out.println( num + " is a prime number");
+       }
+       else{
+                System.out.println( num + " is not a prime number");
+
+       }
+    }
+}
