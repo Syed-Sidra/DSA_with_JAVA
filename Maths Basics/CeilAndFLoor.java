fc6412774c6cecc381c48e8aa4f5870bb469ee1e
Syed-Sidra <sn0255724@gmail.com>
2025-07-31 22:00:54 +0530

    Adding ceil and floor program

---
+++ Maths Basics/CeilAndFLoor.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+public class CeilAndFLoor {
+    public static int ceil(int a, int b){
+        int div = a/b;
+
+        if((a^b)<0 && a/b != 0){
+            div--;
+        }
+        return div;
+    }
+     public static int floor(int a, int b){
+        int  div = a/b;
+
+        if((a^b)>0 && a/b != 0){
+            return div++;
+        }
+        return div;
+    }
+    public static ArrayList<Integer> ceilAndFloor(int a, int b){
+         ArrayList<Integer> res = new ArrayList<>();
+
+         res.add(ceil(a,b));
+         res.add(floor(a,b));
+
+         return res;
+    }
+
+
+    public static void main(String[] args) {
+        int a = -7, b = 2;
+
+        ArrayList<Integer> res = ceilAndFloor(a,b);
+        System.out.print(res.get(0) + " " + res.get(1));
+    }
+}
---
+++ Maths Basics/EvenAndOdd.java
@@ -0,0 +1,11 @@
+class EvenAndOdd{
+    public static void main(String[] args){
+        int n = 12;
+        if(n%2 == 0){
+            System.out.println("even");
+        }
+        else{
+            System.out.print("Odd");
+        }
+    }
+}
